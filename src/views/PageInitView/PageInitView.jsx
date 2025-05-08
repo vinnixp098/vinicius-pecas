399aaef551a69fb0d481e92375e104217ef9974b
Vinicius <vinicius.pires@apotiguar.com.br>
2025-05-08 13:12:35 -0300

    Subindo melhorias no projeto

--- src/views/PageInitView/PageInitView.jsx
+++ src/views/PageInitView/PageInitView.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { HeaderComponent } from "../../components/HeaderComponent/HeaderComponent";
-import { FooterComponent } from "../../components/FooterComponent/FooterComponent";
 import "./PageInitView.css"; 
 
 export function PageInitView() {
@@ -10,7 +9,6 @@ export function PageInitView() {
       <div className="outlet">
         <Outlet />
       </div>
-      {/* <FooterComponent /> */}
     </div>
   );
 }
